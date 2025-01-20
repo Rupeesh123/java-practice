ead8d4c792b1f40a44fb1aaf273fcce4246a2de4
dhanunjaypv <dhanunjay.pv@gmail.com>
2025-01-20 18:36:47 +0530

    dummy

--- src/main/java/com/tcs/practice/App.java
+++ src/main/java/com/tcs/practice/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Rushee!" );
+        System.out.println( "Hello Rushee please practice well all the best!" );
     }
 }
