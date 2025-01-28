ce40e21110eecf13e6ec177d0099d38aa9b85a03
dhanunjaypv <dhanunjay.pv@gmail.com>
2025-01-28 17:16:33 +0530

    variables demo

---
+++ src/main/java/com/tcs/practice/dhanunjay/VariablesDemo.java
@@ -0,0 +1,30 @@
+package com.tcs.practice.dhanunjay;
+
+public class VariablesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=10;
+		int y=200;
+		int z=x-y;
+		System.out.println("hey the sum is "+ z);
+		
+		String name="dhanunjay";
+		
+		System.out.println("Hey "+name+ " how are you");
+		
+		float amount = 20000.25f;
+		System.out.println(amount);
+		
+		boolean isWorking=true;
+		
+		char charector='Y';
+		System.out.println(amount);
+		
+		System.out.println(isWorking);
+		System.out.println(charector);
+
+	}
+
+}
