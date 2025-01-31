68ffe97240afb56ea049102bb818a7cb4bc31919
Rupeesh123 <rupeeshreddykota@gmail.com>
2025-01-31 04:25:07 -0600

    i Practiced

--- src/main/java/com/tcs/practice/rupeesh/VariablesDemoPractice.java
+++ src/main/java/com/tcs/practice/rupeesh/VariablesDemoPractice.java
@@ -4,6 +4,16 @@ public class VariablesDemoPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+int a=100;
+int b=200;
+int c= a+b;
+int d= a-b;
+int e=a*b;
+int f=a%b;;
+System.out.println("The sum is " + c);
+System.out.println("The difference is" + d);
+System.out.println("multiplication is" + e);
+System.out.println("division" + f);
 
 	}
 
