e4f803415c651e37b10de04ae559c49bdc492453
Rupeesh123 <rupeeshreddykota@gmail.com>
2025-02-06 04:02:53 -0600

    if statement practice

---
+++ src/main/java/com/tcs/practice/rupeesh/ControlStatementsDemo.java
@@ -0,0 +1,21 @@
+package com.tcs.practice.rupeesh;
+
+public class ControlStatementsDemo {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 0;
+		int c = a + b;
+
+		if (c > b) {
+			System.out.println("number is positive");
+		} else if (c < b) {
+			System.out.println("number is negative");
+		}
+		if (c == b) {
+			System.out.println("number is zero");
+		}
+	}
+
+}
