625bf168ec869758d52d00279baca93cb56d5c1b
Rupeesh123 <rupeeshreddykota@gmail.com>
2025-02-16 04:30:21 -0600

    switch case

--- src/main/java/com/tcs/practice/rupeesh/ControlStatementsDemo.java
+++ src/main/java/com/tcs/practice/rupeesh/ControlStatementsDemo.java
@@ -4,18 +4,74 @@ public class ControlStatementsDemo {
 
 	public static void main(String[] args) {
 
-		int a = 10;
+	/*	int a = 100;
 		int b = 0;
 		int c = a + b;
 
 		if (c > b) {
 			System.out.println("number is positive");
-		} else if (c < b) {
-			System.out.println("number is negative");
+		} 
+			else if (c < b) {
+		System.out.println("number is negative");
 		}
 		if (c == b) {
 			System.out.println("number is zero");
+		}*/
+		
+		/*string 
+		age = 19
+		System.out.println("enter your age");
+		if (age >= 18)
+		{
+		System.out.println("elgible for vote");
+		}
+		else {
+			System.out.println("not elgible");
+		}
+	}*/
+		
+		
+		
+	
+		int day = 4;
+		switch (day) {
+		case 1:
+			System.out.println("Monday");
+		break;
+		case 2:
+			System.out.println("Tuesday");
+		break;
+		case 3:
+			System.out.println("Wednesday");
+		break;
+		case 4:
+			System.out.println("Thursaday");
+		break;
+		case 5:
+			System.out.println("Friday");
+		break;
+		case 6:
+			System.out.println("Saturday");
+		break;
+		case 7:
+			System.out.println("Sunday");
+		break;
+		default:
+            System.out.println("Invalid input! Please enter a valid day name.");
 		}
 	}
-
 }
+	     
+	     
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
