78c1c509362df15ae68f3cb1709aa128a3293773
dhanunjaypv <dhanunjay.pv@gmail.com>
2025-02-16 21:04:21 +0530

    Arrays Demo

---
+++ src/main/java/com/tcs/practice/dhanunjay/ArraysDemo.java
@@ -0,0 +1,28 @@
+package com.tcs.practice.dhanunjay;
+
+public class ArraysDemo {
+
+	public static void main(String[] args) {
+		
+		
+		String [] names = new String [5];
+		
+		int [] marks= new int[5];
+		
+		names[0]="rushi1";
+		names[1]="rushi2";
+		names[2]="rushi3";
+		names[3]="rushi4";
+	//	names[4]=4.0;
+		names[4]="rushi5";
+		System.out.println(names[5]);
+		
+		
+		
+		
+		/// ARRAYLIST, LinkedList , HASHSET, LINKEDHASHSET, HASHMAP , LINKEDHASHMAP
+		
+		
+		
+	}
+}
--- src/main/java/com/tcs/practice/dhanunjay/IfStatementsDemo.java
+++ src/main/java/com/tcs/practice/dhanunjay/IfStatementsDemo.java
@@ -1,6 +1,6 @@
 package com.tcs.practice.dhanunjay;
 
-public class ControlStatementsDemo {
+public class IfStatementsDemo {
 
 	public static void main(String[] args) {
 
